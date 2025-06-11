267e83cf9524c5df02f4239320431d1bb7ea2b49
grd2006 <garvit2garvit@gmail.com>
2025-06-11 21:42:56 +0530

    removed unwanted files causing errors

--- app/api/uploadthing/route.js
+++
@@ -1,7 +0,0 @@
-// filepath: c:\My Stuff\Filesss\Tailwind\pinterest-2\app\api\uploadthing\route.js
-import { createRouteHandler } from "uploadthing/next";
-import { ourFileRouter } from "./core";
-
-export const { GET, POST } = createRouteHandler({
-  router: ourFileRouter,
-});
